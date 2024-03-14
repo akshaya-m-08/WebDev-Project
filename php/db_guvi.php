85c9fd4a32384099c1e9cffd768fd0921590ee19
akshaya-m-08 <157813068+akshaya-m-08@users.noreply.github.com>
2024-03-14 12:42:07 -0500

    Update db_guvi.php

--- php/db_guvi.php
+++ php/db_guvi.php
@@ -11,7 +11,7 @@ $query_builder = TRUE;
 
 $conn =mysqli_connect($servername, $username, $password, $database);
 
-if ($conn->connect_error) 
+ if (!$conn)
 {
     die("Connection failed: " .mysqli_connect_error());
 }
