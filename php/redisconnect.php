aa3a42ac146e525ba72f8726c642f6251e918630
Akshaya <akshaya@ethanda.com>
2024-03-16 07:23:43 -0500

    modified:   php/redisconnect.php

--- php/redisconnect.php
+++ php/redisconnect.php
@@ -1,15 +1,18 @@
 <?php
+
+$redis = new Predis\Client(getenv('REDIS_URL') . "?ssl[verify_peer_name]=0&ssl[verify_peer]=0");
+
 $url = parse_url(getenv("REDISCLOUD_URL"));
 $host = "redis-17411.c16.us-east-1-3.ec2.cloud.redislabs.com";
 $port = 17411;
 $password = "ZB7bivbf2DQXsIBmVucdDpcerEhHUEtU";
 
-$redis = new Redis();
+$redis_red = new Redis();
 
-$redis->auth($password);
+$redis_red->auth($password);
 
 $options = ["stream" => ["verify_peer" => false, "verify_peer_name" => false]];
 
-$redis->setOptions($options);
+$redis_red->setOptions($options);
 
 ?>
