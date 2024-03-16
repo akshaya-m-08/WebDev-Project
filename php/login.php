d4fca0b0d7aed918d6e18f7abeb18a55a7391c57
Akshaya <akshaya@ethanda.com>
2024-03-16 08:06:58 -0500

    modified:   php/login.php 	modified:   php/redisconnect.php

--- php/login.php
+++ php/login.php
@@ -1,8 +1,10 @@
 <?php
+
+session_start();
+
 include "redisconnect.php";
 
 
-session_start();
 
 
 include "mongo_db.php";
--- php/redisconnect.php
+++ php/redisconnect.php
@@ -4,6 +4,8 @@ require '../vendor/autoload.php'; //
 
 $redis = new Predis\Client(getenv('REDIS_URL') . "?ssl[verify_peer_name]=0&ssl[verify_peer]=0");
 
+ini_set('session.save_handler', 'redis');
+ini_set('session.save_path', 'tcp://redis-17411.c16.us-east-1-3.ec2.cloud.redislabs.com:17411?auth=ZB7bivbf2DQXsIBmVucdDpcerEhHUEtU');
 // Custom session handling functions
 function custom_session_open($save_path, $session_name) {
     global $redis;
