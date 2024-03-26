51f49192058b035ec4616319918e066c0f28b68f
Akshaya <akshaya@ethanda.com>
2024-03-26 03:12:17 -0500

    modified:   php/login.php 	modified:   php/redisconnect.php

--- php/redisconnect.php
+++ php/redisconnect.php
@@ -1,6 +1,6 @@
 <?php
 
-require '../vendor/autoload.php'; //
+require '../vendor/autoload.php'; 
 
 $redis = new Predis\Client(getenv('REDIS_URL') . "?ssl[verify_peer_name]=0&ssl[verify_peer]=0");
 
